1c556315f0348950260fb4f2ad3494b2a844be5e
David Glinski <david.glinski@wexinc.com>
2024-01-16 12:26:46 -0600

    Add Phoenix Components

--- poc/src/app/app.module.ts
+++ poc/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { InputTextModule } from '@wexinc/phoenix-angular-13-components';
+import { ButtonModule } from '@wexinc/phoenix-angular-13-components';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
